e02eac4f14a564457e3aeac896f94b0e2bb17560
Miguel Andrade <miguelangelofariaandrade@gmail.com>
2026-03-11 09:48:02 +0000

    Adicionadas propriedades ao model do cliente

---
+++ WpfProjectLpdsw/WpfProjectLpdsw/Client.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfProjectLpdsw
+{
+    internal class Class1
+    {
+        public int Id { get; set; }
+    }
+}
